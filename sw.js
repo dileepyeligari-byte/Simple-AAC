72a6c213b013b051b94fcfc33c6b3f4b72084c2e
Dileep Yeligari <dileep@aac.app>
2026-03-28 16:29:08 +0530

    feat: Initial release of Simple-AAC PWA with Learning Levels and Child-Friendly UI

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'aac-cache-v5';
+const URLS_TO_CACHE = ['index.html', 'manifest.json', 'sw.js', 'icon-192.png', 'icon-512.png'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
